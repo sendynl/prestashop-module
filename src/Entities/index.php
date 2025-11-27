bbd64b0042131e82a26b4ae4de1aab0445746192
Adriaan Zonnenberg <a.zonnenberg@sendy.nl>
2025-11-27 11:33:34 +0100

    Run autoindex on pre-commit

--- src/Entities/index.php
+++ src/Entities/index.php
@@ -9,6 +9,7 @@
  * @see https://github.com/sendynl/prestashop-module
  */
 declare(strict_types=1);
+
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
